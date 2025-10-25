cb923fdd19d80246a9b56484256cc2ed6e99fd77
Ubuntu <UsuarioSP@MV-004.kbxjhwgr2gruzimispp1x2wiqa.cx.internal.cloudapp.net>
2025-10-25 00:34:35 +0000

    servicio2

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
+# App B runs on port 3000
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    return 'este es el Hola Mundo2', 200
+def hello_world_b():
+    """Endpoint for App B (the callee)."""
+    # This is the message App A will retrieve
+    return 'The message from App B (Port 3000) is: este es el Hola Mundo2', 200
 
 if __name__ == '__main__':
+    # Running on 0.0.0.0 and port 3000
     app.run(host='0.0.0.0', port=3000)
