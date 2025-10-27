334396356854f8e4cda8d1282b6bc9cc6f41dc55
Ubuntu <UsuarioSP@MV-004.kbxjhwgr2gruzimispp1x2wiqa.cx.internal.cloudapp.net>
2025-10-27 23:26:57 +0000

    servicios startup2.2

--- app.py
+++ app.py
@@ -1,38 +1,23 @@
+# app_b.py
 from flask import Flask
 
-# App B runs on port 3000
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello_world_b():
-    """Endpoint for App B (the callee)."""
-    # This is the message App A will retrieve
-    return 'The message from App B (Port 3000) is: este es el Hola Mundo2', 200
+def hello_world():
+    return 'Hola desde app B', 200
 
 @app.route('/health', methods=['GET'])
-def call_app_b():
-    try:
-        # Llamamos a la app B, que está en localhost:3001
-        return 'OK', response.status_code
-    except requests.exceptions.ConnectionError:
-        return 'Error: no se pudo conectar con app B', 500
+def call_health():
+    return 'OK', 200
+
 @app.route('/startup', methods=['GET'])
-def call_app_b():
-    try:
-        # Llamamos a la app B, que está en localhost:3001
-        return 'OK', response.status_code
-    except requests.exceptions.ConnectionError:
-        return 'Error: no se pudo conectar con app B', 500
+def call_startup():
+    return 'OK', 200
+    
 @app.route('/readiness', methods=['GET'])
-def call_app_b():
-    try:
-        # Llamamos a la app B, que está en localhost:3001
-        return 'OK', response.status_code
-    except requests.exceptions.ConnectionError:
-        return 'Error: no se pudo conectar con app B', 500
-
-
+def call_readiness():
+    return 'OK', 200
 
 if __name__ == '__main__':
-    # Running on 0.0.0.0 and port 3000
     app.run(host='0.0.0.0', port=3000)
